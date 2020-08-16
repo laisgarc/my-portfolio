5273e552a747fbd02b2fd9826f0a288d9bcf209c
lais <laisgarcia@usp.br>
2020-08-15 21:52:22 -0300

    menu links

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import Gallery from 'react-grid-gallery';
 import { photos } from './photos.js'
 
-import './styles.css'
 
 export default class Main extends Component {
     
     render() {
         return ( 
-            <Gallery images= { photos } enableImageSelection={ false }/> 
+            <Gallery images= { photos } enableImageSelection={ false }/>
         )
     }
 }
